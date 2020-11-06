4f7c6662599aca99e0a8c592bcf1c5ec74aafa83
lyj00912 <3122742760@qq.com>
2020-11-06 20:03:23 +0800

    refactored and tested FPS.h

--- FPS.h
+++ FPS.h
@@ -15,7 +15,7 @@ int   FPSDeltaTimeCount;
 int   FPSFrameCount;
 char  FPSBuffer[FPS_BUFFER_LENGTH];
 
-void CalcFPS(FrameBuffer fb, int width, int height, int deltaTime) {
+void ClacFPS(FrameBuffer fb, int width, int height, int deltaTime) {
 	/*
 	** FPS Processing
 	*/
@@ -34,9 +34,11 @@ void CalcFPS(FrameBuffer fb, int width, int height, int deltaTime) {
 	sprintf_s(
 		FPSBuffer,
 		FPS_BUFFER_LENGTH,
-		"FPS: %f; DeltaTime: %d",
+		"FPS: %f; DeltaTime: %d; Width: %d; Height: %d",
 		FPS,
-		deltaTime
+		deltaTime,
+		width,
+		height
 	);
 	DrawShadowString(fb, width, height, 10, 10, FPSBuffer);
 }
