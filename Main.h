138c13d62078633a707eb6517deb2b666044d30f
lyj00912 <3122742760@qq.com>
2021-01-31 18:56:28 +0800

    created DrawFlatBottomTriangle()

--- Main.h
+++ Main.h
@@ -4,261 +4,332 @@
 #include "./JackieEngineLayer/JackieEngine.h"
 
 /* Define Window Class Properties */
-#define WindowClassName L"CrossBuffer Class"
-#define WindowTitle     L"CrossBuffer <Click Mouse Buttons or A and D to swap the color>"
+#define WindowClassName L"Jackie Engine Class"
+#define WindowTitle     L"Jackie Engine"
 
-/*
-** Global Variables
-*/
+void DrawHLine(FrameBuffer fb, int x0, int x1, int y, Color color) {
+	for (int i = x0; i < x1; i++) {
+		//SetPixel(fb, i, y, color);
+		((unsigned long*)fb.pBits) [i + (fb.Pitch >> 2) * y] = color;
+	}
+}
 
-Camera4D  cam;
-Vector4D  vecs[8];
-Matrix4D  Mrotation;
-char*     buffer;
-char      realbuffer[1000];
-float     keyboardSensitivity  = 0.005f;
-BOOL      rotateOrNot          = TRUE;
-float     rotateDegree         = 0.0f;
-int       rotateKeyDelay       = 0;
+void DrawFlatBottomTriangle(FrameBuffer fb, int x0, int yStart, int x1, int x2, int yEnd, Color color) {
+	double l1m = 1.0f * (x1 - x0) / (yEnd - yStart);
+	double l2m = 1.0f * (x2 - x0) / (yEnd - yStart);
 
+	double i = 1.0f * x0;
+	double j = 1.0f * x0;
 
-/*
-** Setup()
-*/
+	for (int yHat = yStart; yHat <= yEnd; yHat++) {
+		DrawHLine(fb, (int)i, (int)j, yHat, color);
+		i += l1m;
+		j += l2m;
+	}
+}
 
 void Setup(FrameBuffer fb, Keyboard kb, int deltaTime) {
-	
-	
-	/*
-	** Camera
-	*/
-
-	cam = CreateCamera4D(
-		0.0f, 0.0f, 7.0f, 0.0f, 0.0f, 0.0f,
-		-0.1f, -1000.0f, 60.0f, fb.Width, fb.Height
-	);
-	CalcCamera4DMatrices(&cam);
-
-
-	/*
-	** Vectors
-	*/
-
-	vecs[0] = CreateVector4D(-1.0f, -1.0f,  1.0f, 1.0f);
-	vecs[1] = CreateVector4D( 1.0f, -1.0f,  1.0f, 1.0f);
-	vecs[2] = CreateVector4D( 1.0f,  1.0f,  1.0f, 1.0f);
-	vecs[3] = CreateVector4D(-1.0f,  1.0f,  1.0f, 1.0f);
-	vecs[4] = CreateVector4D(-1.0f, -1.0f, -1.0f, 1.0f);
-	vecs[5] = CreateVector4D( 1.0f, -1.0f, -1.0f, 1.0f);
-	vecs[6] = CreateVector4D( 1.0f,  1.0f, -1.0f, 1.0f);
-	vecs[7] = CreateVector4D(-1.0f,  1.0f, -1.0f, 1.0f);
+	;
 }
 
-
-/*
-** Update()
-*/
+int x0 = 500;
+int yStart = 100;
+int x1 = 0;
+int x2 = 1000;
+int yEnd = 200;
 
 void Update(FrameBuffer fb, Keyboard kb, int deltaTime) {
-	
+	DrawFlatBottomTriangle(fb, x0, yStart, x1, x2, yEnd, CreateColor(255, 255, 255));
 
-	/*
-	** Calculating FPS
-	*/
+	if (kb['W']) {
+		yStart -= 3;
+	}
+	if (kb['S']) {
+		yStart += 3;
+	}
+	if (kb['A']) {
+		x0 -= 3;
+	}
+	if (kb['D']) {
+		x0 += 3;
+	}
+
+	if (kb['F']) {
+		x1 -= 3;
+	}
+	if (kb['G']) {
+		x1 += 3;
+	}
+
+	if (kb['H']) {
+		x2 -= 3;
+	}
+	if (kb['J']) {
+		x2 += 3;
+	}
+
+	if (kb['I']) {
+		yEnd -= 3;
+	}
+	if (kb['K']) {
+		yEnd += 3;
+	}
 
 	CalcFPS(fb, deltaTime);
-
-
-	/*
-	** Position Responder
-	*/
-
-	if (kb['W'] == TRUE) {
-		cam.position.z -= 1.0f * keyboardSensitivity * deltaTime;
-	}
-	if (kb['S'] == TRUE) {
-		cam.position.z += 1.0f * keyboardSensitivity * deltaTime;
-	}
-
-	if (kb['A'] == TRUE) {
-		cam.position.x -= 0.5f * keyboardSensitivity * deltaTime;
-	}
-	if (kb['D'] == TRUE) {
-		cam.position.x += 0.5f * keyboardSensitivity * deltaTime;
-	}
-
-	if (kb['E'] == TRUE) {
-		cam.position.y -= 0.5f * keyboardSensitivity * deltaTime;
-	}
-	if (kb['Q'] == TRUE) {
-		cam.position.y += 0.5f * keyboardSensitivity * deltaTime;
-	}
-
-
-	/*
-	** Rotation Responder
-	*/
-
-	if (kb['I'] == TRUE) {
-		cam.rotation.x -= 5.0f * keyboardSensitivity * deltaTime;
-	}
-	if (kb['K'] == TRUE) {
-		cam.rotation.x += 5.0f * keyboardSensitivity * deltaTime;
-	}
-
-	if (kb['J'] == TRUE) {
-		cam.rotation.y -= 5.0f * keyboardSensitivity * deltaTime;
-	}
-	if (kb['L'] == TRUE) {
-		cam.rotation.y += 5.0f * keyboardSensitivity * deltaTime;
-	}
-
-	if (kb['U'] == TRUE) {
-		cam.rotation.z -= 5.0f * keyboardSensitivity * deltaTime;
-	}
-	if (kb['O'] == TRUE) {
-		cam.rotation.z += 5.0f * keyboardSensitivity * deltaTime;
-	}
-
-
-	/*
-	** R Key Responder
-	*/
-
-	if (kb['R'] == TRUE && rotateKeyDelay == 0) {
-		rotateOrNot = !(rotateOrNot);
-		rotateKeyDelay = 10;
-	}
-
-	if (rotateKeyDelay > 0) {
-		rotateKeyDelay -= 1;
-	}
-
-
-	/*
-	** Refresh Camera Matrices
-	*/
-
-	RefreshCamera4DMatrices(&cam);
-
-
-	/*
-	** Rotate the Vectors
-	*/
-
-	Mrotation = CreateRotationMatrix(rotateDegree, -30.0f, rotateDegree);
-
-	if (rotateOrNot == TRUE) {
-		rotateDegree += 0.03f * deltaTime;
-	}
-	if (rotateDegree >= 360.0f) {
-		rotateDegree =  0.0f;
-	}
-
-	for (int i = 0; i < 8; i++) {
-		vecs[i] = Vector4DTimesMatrix4D(
-			&(vecs[i]), &(Mrotation)
-		);
-	}
-
-
-	/*
-	** Do Projection
-	*/
-
-	for (int i = 0; i < 8; i++) {
-		vecs[i] = Vector4DTimesMatrix4D(
-			&(vecs[i]), &(cam.Mtransform)
-		);
-	}
-
-	for (int i = 0; i < 8; i++) {
-		Vector4DDevidedByW(&(vecs[i]));
-	}
-
-
-	/*
-	** Output Things
-	*/
-
-	// Camera Frustum
-	sprintf_s(
-		realbuffer, 1000,
-		"n:%f\nf:%f\nt:%f\nb:%f\nl:%f\nr:%f\n",
-		cam.n, cam.f, cam.t, cam.b, cam.l, cam.r
-	);
-	DrawShadowString(fb, 10, 42, realbuffer);
-
-	// Camera Position
-	buffer = OutputVector4D(&(cam.position));
-	DrawShadowString(fb, 10, 314, buffer);
-	free(buffer);
-
-	// Camera Rotation
-	buffer = OutputVector4D(&(cam.rotation));
-	DrawShadowString(fb, 10, 346, buffer);
-	free(buffer);
-
-	// Mtransform
-	buffer = OutputMatrix4D(&(cam.Mtransform));
-	DrawShadowString(fb, 10, 378, buffer);
-	free(buffer);
-
-	// Vector List
-	for (int i = 0; i < 8; i++) {
-		buffer = OutputVector4D(&(vecs[i]));
-		DrawShadowString(fb, 10, 154 + i * 16, buffer);
-		free(buffer);
-	}
-
-
-	/*
-	** Get And Clear Z-Buffer
-	*/
-
-	float* ZBuffer = (float*)malloc(sizeof(float) * fb.Width * fb.Height);
-	for (int y = 0; y < fb.Height; y++) {
-		for (int x = 0; x < fb.Width; x++) {
-			ZBuffer[y * fb.Width + x] = cam.f;
-		}
-	}
-
-
-	/*
-	** Do Rasterization
-	*/
-
-	DrawFlatMesh4D(fb, &(vecs[0]), &(vecs[1]), &(vecs[3]), CreateColor(255, 0, 255, 255), ZBuffer);
-	DrawFlatMesh4D(fb, &(vecs[2]), &(vecs[3]), &(vecs[1]), CreateColor(255, 255, 0, 255), ZBuffer);
-	DrawFlatMesh4D(fb, &(vecs[5]), &(vecs[4]), &(vecs[6]), CreateColor(255, 255, 255, 255), ZBuffer);
-	DrawFlatMesh4D(fb, &(vecs[7]), &(vecs[6]), &(vecs[4]), CreateColor(255, 255, 255, 255), ZBuffer);
-	DrawFlatMesh4D(fb, &(vecs[4]), &(vecs[0]), &(vecs[7]), CreateColor(255, 255, 255, 255), ZBuffer);
-	DrawFlatMesh4D(fb, &(vecs[3]), &(vecs[7]), &(vecs[0]), CreateColor(255, 255, 255, 255), ZBuffer);
-	//DrawFlatMesh4D(fb, &(vecs[1]), &(vecs[5]), &(vecs[2]), CreateColor(255, 255, 255, 255), ZBuffer);
-	//DrawFlatMesh4D(fb, &(vecs[6]), &(vecs[2]), &(vecs[5]), CreateColor(255, 255, 255, 255), ZBuffer);
-	//DrawFlatMesh4D(fb, &(vecs[3]), &(vecs[2]), &(vecs[7]), CreateColor(255, 255, 255, 255), ZBuffer);
-	//DrawFlatMesh4D(fb, &(vecs[6]), &(vecs[7]), &(vecs[2]), CreateColor(255, 255, 255, 255), ZBuffer);
-	//DrawFlatMesh4D(fb, &(vecs[5]), &(vecs[0]), &(vecs[4]), CreateColor(255, 255, 255, 255), ZBuffer);
-	//DrawFlatMesh4D(fb, &(vecs[5]), &(vecs[1]), &(vecs[0]), CreateColor(255, 255, 255, 255), ZBuffer);
-
-
-	/*
-	** Reset Vectors
-	*/
-
-	vecs[0] = CreateVector4D(-1.0f, -1.0f,  1.0f, 1.0f);
-	vecs[1] = CreateVector4D( 1.0f, -1.0f,  1.0f, 1.0f);
-	vecs[2] = CreateVector4D( 1.0f,  1.0f,  1.0f, 1.0f);
-	vecs[3] = CreateVector4D(-1.0f,  1.0f,  1.0f, 1.0f);
-	vecs[4] = CreateVector4D(-1.0f, -1.0f, -1.0f, 1.0f);
-	vecs[5] = CreateVector4D( 1.0f, -1.0f, -1.0f, 1.0f);
-	vecs[6] = CreateVector4D( 1.0f,  1.0f, -1.0f, 1.0f);
-	vecs[7] = CreateVector4D(-1.0f,  1.0f, -1.0f, 1.0f);
-
-
-	/*
-	** Delete Z-Buffer
-	*/
-
-	free(ZBuffer);
 }
+
+///*
+//** Global Variables
+//*/
+//
+//Camera4D  cam;
+//Vector4D  vecs[8];
+//Matrix4D  Mrotation;
+//char*     buffer;
+//char      realbuffer[1000];
+//float     keyboardSensitivity  = 0.005f;
+//BOOL      rotateOrNot          = TRUE;
+//float     rotateDegree         = 0.0f;
+//int       rotateKeyDelay       = 0;
+//
+//
+///*
+//** Setup()
+//*/
+//
+//void Setup(FrameBuffer fb, Keyboard kb, int deltaTime) {
+//	
+//	
+//	/*
+//	** Camera
+//	*/
+//
+//	cam = CreateCamera4D(
+//		0.0f, 0.0f, 7.0f, 0.0f, 0.0f, 0.0f,
+//		-0.1f, -1000.0f, 60.0f, fb.Width, fb.Height
+//	);
+//	CalcCamera4DMatrices(&cam);
+//
+//
+//	/*
+//	** Vectors
+//	*/
+//
+//	vecs[0] = CreateVector4D(-1.0f, -1.0f,  1.0f, 1.0f);
+//	vecs[1] = CreateVector4D( 1.0f, -1.0f,  1.0f, 1.0f);
+//	vecs[2] = CreateVector4D( 1.0f,  1.0f,  1.0f, 1.0f);
+//	vecs[3] = CreateVector4D(-1.0f,  1.0f,  1.0f, 1.0f);
+//	vecs[4] = CreateVector4D(-1.0f, -1.0f, -1.0f, 1.0f);
+//	vecs[5] = CreateVector4D( 1.0f, -1.0f, -1.0f, 1.0f);
+//	vecs[6] = CreateVector4D( 1.0f,  1.0f, -1.0f, 1.0f);
+//	vecs[7] = CreateVector4D(-1.0f,  1.0f, -1.0f, 1.0f);
+//}
+//
+//
+///*
+//** Update()
+//*/
+//
+//void Update(FrameBuffer fb, Keyboard kb, int deltaTime) {
+//	
+//
+//	/*
+//	** Calculating FPS
+//	*/
+//
+//	CalcFPS(fb, deltaTime);
+//
+//
+//	/*
+//	** Position Responder
+//	*/
+//
+//	if (kb['W'] == TRUE) {
+//		cam.position.z -= 1.0f * keyboardSensitivity * deltaTime;
+//	}
+//	if (kb['S'] == TRUE) {
+//		cam.position.z += 1.0f * keyboardSensitivity * deltaTime;
+//	}
+//
+//	if (kb['A'] == TRUE) {
+//		cam.position.x -= 0.5f * keyboardSensitivity * deltaTime;
+//	}
+//	if (kb['D'] == TRUE) {
+//		cam.position.x += 0.5f * keyboardSensitivity * deltaTime;
+//	}
+//
+//	if (kb['E'] == TRUE) {
+//		cam.position.y -= 0.5f * keyboardSensitivity * deltaTime;
+//	}
+//	if (kb['Q'] == TRUE) {
+//		cam.position.y += 0.5f * keyboardSensitivity * deltaTime;
+//	}
+//
+//
+//	/*
+//	** Rotation Responder
+//	*/
+//
+//	if (kb['I'] == TRUE) {
+//		cam.rotation.x -= 5.0f * keyboardSensitivity * deltaTime;
+//	}
+//	if (kb['K'] == TRUE) {
+//		cam.rotation.x += 5.0f * keyboardSensitivity * deltaTime;
+//	}
+//
+//	if (kb['J'] == TRUE) {
+//		cam.rotation.y -= 5.0f * keyboardSensitivity * deltaTime;
+//	}
+//	if (kb['L'] == TRUE) {
+//		cam.rotation.y += 5.0f * keyboardSensitivity * deltaTime;
+//	}
+//
+//	if (kb['U'] == TRUE) {
+//		cam.rotation.z -= 5.0f * keyboardSensitivity * deltaTime;
+//	}
+//	if (kb['O'] == TRUE) {
+//		cam.rotation.z += 5.0f * keyboardSensitivity * deltaTime;
+//	}
+//
+//
+//	/*
+//	** R Key Responder
+//	*/
+//
+//	if (kb['R'] == TRUE && rotateKeyDelay == 0) {
+//		rotateOrNot = !(rotateOrNot);
+//		rotateKeyDelay = 10;
+//	}
+//
+//	if (rotateKeyDelay > 0) {
+//		rotateKeyDelay -= 1;
+//	}
+//
+//
+//	/*
+//	** Refresh Camera Matrices
+//	*/
+//
+//	RefreshCamera4DMatrices(&cam);
+//
+//
+//	/*
+//	** Rotate the Vectors
+//	*/
+//
+//	Mrotation = CreateRotationMatrix(rotateDegree, -30.0f, rotateDegree);
+//
+//	if (rotateOrNot == TRUE) {
+//		rotateDegree += 0.03f * deltaTime;
+//	}
+//	if (rotateDegree >= 360.0f) {
+//		rotateDegree =  0.0f;
+//	}
+//
+//	for (int i = 0; i < 8; i++) {
+//		vecs[i] = Vector4DTimesMatrix4D(
+//			&(vecs[i]), &(Mrotation)
+//		);
+//	}
+//
+//
+//	/*
+//	** Do Projection
+//	*/
+//
+//	for (int i = 0; i < 8; i++) {
+//		vecs[i] = Vector4DTimesMatrix4D(
+//			&(vecs[i]), &(cam.Mtransform)
+//		);
+//	}
+//
+//	for (int i = 0; i < 8; i++) {
+//		Vector4DDevidedByW(&(vecs[i]));
+//	}
+//
+//
+//	/*
+//	** Output Things
+//	*/
+//
+//	// Camera Frustum
+//	sprintf_s(
+//		realbuffer, 1000,
+//		"n:%f\nf:%f\nt:%f\nb:%f\nl:%f\nr:%f\n",
+//		cam.n, cam.f, cam.t, cam.b, cam.l, cam.r
+//	);
+//	DrawShadowString(fb, 10, 42, realbuffer);
+//
+//	// Camera Position
+//	buffer = OutputVector4D(&(cam.position));
+//	DrawShadowString(fb, 10, 314, buffer);
+//	free(buffer);
+//
+//	// Camera Rotation
+//	buffer = OutputVector4D(&(cam.rotation));
+//	DrawShadowString(fb, 10, 346, buffer);
+//	free(buffer);
+//
+//	// Mtransform
+//	buffer = OutputMatrix4D(&(cam.Mtransform));
+//	DrawShadowString(fb, 10, 378, buffer);
+//	free(buffer);
+//
+//	// Vector List
+//	for (int i = 0; i < 8; i++) {
+//		buffer = OutputVector4D(&(vecs[i]));
+//		DrawShadowString(fb, 10, 154 + i * 16, buffer);
+//		free(buffer);
+//	}
+//
+//
+//	/*
+//	** Get And Clear Z-Buffer
+//	*/
+//
+//	float* ZBuffer = (float*)malloc(sizeof(float) * fb.Width * fb.Height);
+//	for (int y = 0; y < fb.Height; y++) {
+//		for (int x = 0; x < fb.Width; x++) {
+//			ZBuffer[y * fb.Width + x] = cam.f;
+//		}
+//	}
+//
+//
+//	/*
+//	** Do Rasterization
+//	*/
+//
+//	DrawFlatMesh4D(fb, &(vecs[0]), &(vecs[1]), &(vecs[3]), CreateColor(255, 0, 255, 255), ZBuffer);
+//	DrawFlatMesh4D(fb, &(vecs[2]), &(vecs[3]), &(vecs[1]), CreateColor(255, 255, 0, 255), ZBuffer);
+//	DrawFlatMesh4D(fb, &(vecs[5]), &(vecs[4]), &(vecs[6]), CreateColor(255, 255, 255, 255), ZBuffer);
+//	DrawFlatMesh4D(fb, &(vecs[7]), &(vecs[6]), &(vecs[4]), CreateColor(255, 255, 255, 255), ZBuffer);
+//	DrawFlatMesh4D(fb, &(vecs[4]), &(vecs[0]), &(vecs[7]), CreateColor(255, 255, 255, 255), ZBuffer);
+//	DrawFlatMesh4D(fb, &(vecs[3]), &(vecs[7]), &(vecs[0]), CreateColor(255, 255, 255, 255), ZBuffer);
+//	//DrawFlatMesh4D(fb, &(vecs[1]), &(vecs[5]), &(vecs[2]), CreateColor(255, 255, 255, 255), ZBuffer);
+//	//DrawFlatMesh4D(fb, &(vecs[6]), &(vecs[2]), &(vecs[5]), CreateColor(255, 255, 255, 255), ZBuffer);
+//	//DrawFlatMesh4D(fb, &(vecs[3]), &(vecs[2]), &(vecs[7]), CreateColor(255, 255, 255, 255), ZBuffer);
+//	//DrawFlatMesh4D(fb, &(vecs[6]), &(vecs[7]), &(vecs[2]), CreateColor(255, 255, 255, 255), ZBuffer);
+//	//DrawFlatMesh4D(fb, &(vecs[5]), &(vecs[0]), &(vecs[4]), CreateColor(255, 255, 255, 255), ZBuffer);
+//	//DrawFlatMesh4D(fb, &(vecs[5]), &(vecs[1]), &(vecs[0]), CreateColor(255, 255, 255, 255), ZBuffer);
+//
+//
+//	/*
+//	** Reset Vectors
+//	*/
+//
+//	vecs[0] = CreateVector4D(-1.0f, -1.0f,  1.0f, 1.0f);
+//	vecs[1] = CreateVector4D( 1.0f, -1.0f,  1.0f, 1.0f);
+//	vecs[2] = CreateVector4D( 1.0f,  1.0f,  1.0f, 1.0f);
+//	vecs[3] = CreateVector4D(-1.0f,  1.0f,  1.0f, 1.0f);
+//	vecs[4] = CreateVector4D(-1.0f, -1.0f, -1.0f, 1.0f);
+//	vecs[5] = CreateVector4D( 1.0f, -1.0f, -1.0f, 1.0f);
+//	vecs[6] = CreateVector4D( 1.0f,  1.0f, -1.0f, 1.0f);
+//	vecs[7] = CreateVector4D(-1.0f,  1.0f, -1.0f, 1.0f);
+//
+//
+//	/*
+//	** Delete Z-Buffer
+//	*/
+//
+//	free(ZBuffer);
+//}
