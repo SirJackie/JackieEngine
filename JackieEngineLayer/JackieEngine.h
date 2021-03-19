711cd5d4e384405dece32e0a578eb6c2d1f03a3e
lyj00912 <3122742760@qq.com>
2021-03-19 23:17:26 +0800

    save changes of triangle functions

--- JackieEngineLayer/JackieEngine.h
+++ JackieEngineLayer/JackieEngine.h
@@ -1,9 +1,9 @@
 #ifndef __JKEG
 #define __JKEG
 
-#include "LinearAlgebra.h"
-#include "CameraSupport.h"
-#include "ModelSupport.h"
-#include "Rasterization.h"
+#include "MathSupport.h"
+//#include "CameraSupport.h"
+//#include "ModelSupport.h"
+//#include "Rasterization.h"
 
 #endif
--- Main.h
+++ Main.h
@@ -17,22 +17,51 @@ using std::stringstream;
 */
 
 void Setup(FrameBuffer& fb, Keyboard kb, int deltaTime, vector<FrameBuffer*>& fbLoadingQueue) {
-	//vecs[0] = CreateVector4D(-1.0, -1.0,  1.0, 1.0);
-	//vecs[1] = CreateVector4D( 1.0, -1.0,  1.0, 1.0);
-	//vecs[2] = CreateVector4D( 1.0,  1.0,  1.0, 1.0);
-	//vecs[3] = CreateVector4D(-1.0,  1.0,  1.0, 1.0);
-	//vecs[4] = CreateVector4D(-1.0, -1.0, -1.0, 1.0);
-	//vecs[5] = CreateVector4D( 1.0, -1.0, -1.0, 1.0);
-	//vecs[6] = CreateVector4D( 1.0,  1.0, -1.0, 1.0);
-	//vecs[7] = CreateVector4D(-1.0,  1.0, -1.0, 1.0);
+	;
 }
 
+
+FPSCalculator fps;
 
 /*
 ** Update()
 */
 
 void Update(FrameBuffer& fb, Keyboard kb, int deltaTime, vector<FrameBuffer*>& fbLoadingQueue) {
+	fps.Count(deltaTime);
+	fps.ShowOnScreen(fb);
+
+	stringstream ss;
+
+	/*ss << sind(30.0f);
+	ss << ";";
+	ss << cosd(60.0f);
+	ss << ";";
+	ss << tand(45.0f);
+	ss << ";";
+
+	ss << asind(0.5f);
+	ss << ";";
+	ss << acosd(0.5f);
+	ss << ";";
+	ss << atand(1.0f);
+	ss << ";";*/
+
+	float i = cos(30.0f);
+	ss << i;
+
+	fb.Draw(ss.str().c_str());
+}
+
+
+
+
+
+
+
+
+
+
 	//vecs[0] = CreateVector4D(-1.0, -1.0,  1.0, 1.0);
 	//vecs[1] = CreateVector4D( 1.0, -1.0,  1.0, 1.0);
 	//vecs[2] = CreateVector4D( 1.0,  1.0,  1.0, 1.0);
@@ -41,4 +70,3 @@ void Update(FrameBuffer& fb, Keyboard kb, int deltaTime, vector<FrameBuffer*>& f
 	//vecs[5] = CreateVector4D( 1.0, -1.0, -1.0, 1.0);
 	//vecs[6] = CreateVector4D( 1.0,  1.0, -1.0, 1.0);
 	//vecs[7] = CreateVector4D(-1.0,  1.0, -1.0, 1.0);
-}
