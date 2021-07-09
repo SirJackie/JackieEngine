44878548602975e97ca5450d892c47c7c7a7f9d9
lyj00912 <3122742760@qq.com>
2021-07-09 19:49:37 +0800

    refactored Main.h

--- Main.cpp
+++ Main.cpp
@@ -1,20 +1,20 @@
 #include "Main.h"
 
 CS_FPSCalculator  fpsCalculator;
-FObject           obj;
+FObject           object;
 FCamera           camera;
 FRasterizer       rasterizer;
 
 
 void Setup (CS_FrameBuffer& fb, CS_Keyboard& kb, CS_Mouse& mouse, i32 deltaTime) {
-	obj.Add(FVector4D(-1.0, -1.0, 1.0, 1.0));
-	obj.Add(FVector4D(1.0, -1.0, 1.0, 1.0));
-	obj.Add(FVector4D(1.0, 1.0, 1.0, 1.0));
-	obj.Add(FVector4D(-1.0, 1.0, 1.0, 1.0));
-	obj.Add(FVector4D(-1.0, -1.0, -1.0, 1.0));
-	obj.Add(FVector4D(1.0, -1.0, -1.0, 1.0));
-	obj.Add(FVector4D(1.0, 1.0, -1.0, 1.0));
-	obj.Add(FVector4D(-1.0, 1.0, -1.0, 1.0));
+	object.Add(FVector4D(-1.0, -1.0,  1.0, 1.0));
+	object.Add(FVector4D( 1.0, -1.0,  1.0, 1.0));
+	object.Add(FVector4D( 1.0,  1.0,  1.0, 1.0));
+	object.Add(FVector4D(-1.0,  1.0,  1.0, 1.0));
+	object.Add(FVector4D(-1.0, -1.0, -1.0, 1.0));
+	object.Add(FVector4D( 1.0, -1.0, -1.0, 1.0));
+	object.Add(FVector4D( 1.0,  1.0, -1.0, 1.0));
+	object.Add(FVector4D(-1.0,  1.0, -1.0, 1.0));
 }
 
 void Update(CS_FrameBuffer& fb, CS_Keyboard& kb, CS_Mouse& mouse, i32 deltaTime) {
@@ -25,16 +25,8 @@ void Update(CS_FrameBuffer& fb, CS_Keyboard& kb, CS_Mouse& mouse, i32 deltaTime)
 		0.0f, 0.0f, 10.0f, 0.0f, 0.0f, 0.0f,
 		-0.1f, -1000.0f, 60.0f, fb.width, fb.height
 	);
-
 	rasterizer = FRasterizer(fb);
-	
-	//stringstream ss;
-	//ss << "Camera4D Testing: \n";
-	//ss << camera.str();
-	//ss << obj.vl.size();
-	//ss << "\n";
-	//fb.PrintLn(ss.str().c_str());
 
-	camera.ProjectObject(obj);
-	rasterizer.DrawPoint(obj);
+	camera.ProjectObject(object);
+	rasterizer.DrawPoint(object);
 }
