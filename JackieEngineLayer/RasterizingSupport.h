eee693e7e0f691adf3b19943ef83d453354d0f89
lyj00912 <3122742760@qq.com>
2021-07-10 15:06:48 +0800

    successfully drawn a flat top triangle

--- JackieEngineLayer/RasterizingSupport.h
+++ JackieEngineLayer/RasterizingSupport.h
@@ -16,8 +16,8 @@ public:
 	void DrawProtectedCube(i32 x0, i32 y0, i32 x1, i32 y1, ui8 r_, ui8 g_, ui8 b_);
 	void DrawRadiusCube(i32 x, i32 y, i32 radius);
 	void DrawTriangle(const FVector4D& v0, const FVector4D& v1, const FVector4D& v2);
-	void DrawFlatTopTriangle(f32 yStart, f32 yEnd, f32 x0_, f32 x1_, f32 x2_, ui8 r, ui8 g, ui8 b);
 	void DrawFlatBottomTriangle(f32 yStart, f32 yEnd, f32 x0_, f32 x1_, f32 x2_, ui8 r, ui8 g, ui8 b);
+	void DrawFlatTopTriangle(f32 yStart, f32 yEnd, f32 x0_, f32 x1_, f32 x2_, ui8 r, ui8 g, ui8 b);
 
 	void Draw3DPoint(FVector3D& point);
 	void Draw4DPoint(FVector4D& point);
