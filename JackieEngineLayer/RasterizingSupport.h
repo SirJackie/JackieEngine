db32a448dd5baada18235aeee269b0b581420aad
lyj00912 <3122742760@qq.com>
2022-08-14 15:16:13 +0800

    created wireframe 3d demo

--- JackieEngineLayer/RasterizingSupport.h
+++ JackieEngineLayer/RasterizingSupport.h
@@ -6,6 +6,36 @@
 #include "MathSupport.h"
 #include "ObjectSupport.h"
 
-// Empty
+
+class PubeScreenTransformer
+{
+public:
+	PubeScreenTransformer()
+		:
+		xFactor(0.0f),
+		yFactor(0.0f)
+	{}
+
+	void SetWidthHeight(int screenWidth, int screenHeight)
+	{
+		xFactor = float(screenWidth) / 2.0f;
+		yFactor = float(screenHeight) / 2.0f;
+	}
+
+	Vec3& Transform(Vec3& v) const
+	{
+		const float zInv = 1.0f / v.z;
+		v.x = (v.x * zInv + 1.0f) * xFactor;
+		v.y = (-v.y * zInv + 1.0f) * yFactor;
+		return v;
+	}
+	Vec3 GetTransformed(const Vec3& v) const
+	{
+		return Transform(Vec3(v));
+	}
+private:
+	float xFactor;
+	float yFactor;
+};
 
 #endif
--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,70 @@
 #include "Main.h"
 #include "JackieEngineLayer/MathSupport.h"
 
+const float dt = 1.0f / 60.0f;
+static constexpr float dTheta = PI;
+float offset_z = 2.0f;
+float theta_x = 0.0f;
+float theta_y = 0.0f;
+float theta_z = 0.0f;
+Cube cube(1.0f);
+PubeScreenTransformer pst;
+
 void Setup (CS_FrameBuffer& fb, CS_Keyboard& kb, CS_Mouse& mouse, i32 deltaTime) {
-	
+	pst.SetWidthHeight(fb.width, fb.height);
 }
 
 void Update(CS_FrameBuffer& fb, CS_Keyboard& kb, CS_Mouse& mouse, i32 deltaTime) {
-	Vec3 vec(1.0f, 1.0f, 1.0f);
+	
+	// Keyboard Control
 
-	Mat3 mat(
-		1.0f, 2.0f, 3.0f,
-		4.0f, 5.0f, 6.0f,
-		7.0f, 8.0f, 9.0f
-	);
+	if (kb.IsKeyPressed('Q'))
+	{
+		theta_x = theta_x + dTheta * dt;
+	}
+	if (kb.IsKeyPressed('W'))
+	{
+		theta_y = theta_y + dTheta * dt;
+	}
+	if (kb.IsKeyPressed('E'))
+	{
+		theta_z = theta_z + dTheta * dt;
+	}
+	if (kb.IsKeyPressed('A'))
+	{
+		theta_x = theta_x - dTheta * dt;
+	}
+	if (kb.IsKeyPressed('S'))
+	{
+		theta_y = theta_y - dTheta * dt;
+	}
+	if (kb.IsKeyPressed('D'))
+	{
+		theta_z = theta_z - dTheta * dt;
+	}
+	if (kb.IsKeyPressed('R'))
+	{
+		offset_z += 2.0f * dt;
+	}
+	if (kb.IsKeyPressed('F'))
+	{
+		offset_z -= 2.0f * dt;
+	}
 
-	Vec3 result1 = vec * mat;
-	Vec3 result2 = vec;
-	result2 *= mat;
+	// 3D Transform
 
-	int a = 0;
+	auto lines = cube.GetLines();
+
+	Mat3 rotation =
+		Mat3::RotationX(theta_x) *
+		Mat3::RotationY(theta_y) *
+		Mat3::RotationZ(theta_z);
+
+	for (auto& v : lines.vertices) {
+		v *= rotation;
+		v += Vec3(0.0f, 0.0f, offset_z);
+		pst.Transform(v);
+
+		fb.PutPixel((int)v.x, (int)v.y, 255, 255, 255);
+	}
 }
